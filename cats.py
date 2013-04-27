c0a0eb20968451e21f2359e1df2dfae07b55bf80
Ryan Endacott <rzeg24@gmail.com>
2013-04-27 00:59:36 -0500

    adding dropbox stuff

--- cats.py
+++ cats.py
@@ -1,9 +1,34 @@
+import os
 from flask import Flask
+from dropbox import client, session
+from collections import defaultdict
 app = Flask(__name__)
 
+
+def try_to_load_settings():
+  try:
+    import settings
+  except ImportError:
+    return defaultdict(None)
+  else:
+    return settings
+
+# Load Environment
+settings = try_to_load_settings()
+
+APP_KEY = settings.APP_KEY or os.environ['APP_KEY']
+APP_SECRET = settings.APP_SECRET or os.environ['APP_SECRET']
+ACCESS_TYPE = settings.ACCESS_TYPE or os.environ['ACCESS_TYPE']
+TOKEN_KEY = settings.TOKEN_KEY or os.environ['TOKEN_KEY']
+TOKEN_SECRET = settings.TOKEN_SECRET or os.environ['TOKEN_SECRET']
+
+sess = session.DropboxSession(APP_KEY,APP_SECRET, ACCESS_TYPE )
+sess.set_token(TOKEN_KEY, TOKEN_SECRET)
+client = client.DropboxClient(sess)
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def random_cat():
+    return 'hi'
 
 if __name__ == '__main__':
     app.run()
